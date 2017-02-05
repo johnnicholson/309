0d1b5daff6a6943a3a2dedb5365988187660edd3
Salonee Thanawala <salonee@pcp046744pcs.wireless.calpoly.edu>
2017-02-05 15:32:02 -0800

    Delete test method has been added.

--- src/test/java/RoomType/RoomTypeTest.java
+++ src/test/java/RoomType/RoomTypeTest.java
@@ -24,67 +24,82 @@ import model.RoomType;
 import model.Person.Role;
 
 public class RoomTypeTest {
-  static PersonDAO prsDAO;
-  static RoomTypeDAO rtDAO;
-  
-  MockHttpServletResponse res;
-  MockHttpServletRequest req;
-  People people;
+	static PersonDAO prsDAO;
+	static RoomTypeDAO rtDAO;
 
-  @BeforeClass
-  public static void topSetup() {
-    DAOFactory fact = TestRunner.getMockFact();
-    prsDAO = mock(PersonDAO.class);
-    rtDAO = mock(RoomTypeDAO.class);
-    when(fact.getPersonDAO()).thenReturn(prsDAO);
-    when(fact.getRoomTypeDAO()).thenReturn(rtDAO);
+	MockHttpServletResponse res;
+	MockHttpServletRequest req;
+	People people;
 
-    HibernateUtil.setDAOFactory(fact);
-    org.hibernate.Session ssn = mock(org.hibernate.Session.class);
-    org.hibernate.Transaction transaction = mock(org.hibernate.Transaction.class);
-    SessionFactory sfact = mock(SessionFactory.class);
-    when(sfact.getCurrentSession()).thenReturn(ssn);
-    when(ssn.getTransaction()).thenReturn(transaction);
-    HibernateUtil.setFactory(sfact);
-  }
-  
-  @Before
-  public void setup() {
-    res = new MockHttpServletResponse();
-    req = mock(MockHttpServletRequest.class);
-  }
-  
-  @Test
-  public void RoomTypeGetTest() {
+	@BeforeClass
+	public static void topSetup() {
+		DAOFactory fact = TestRunner.getMockFact();
+		prsDAO = mock(PersonDAO.class);
+		rtDAO = mock(RoomTypeDAO.class);
+		when(fact.getPersonDAO()).thenReturn(prsDAO);
+		when(fact.getRoomTypeDAO()).thenReturn(rtDAO);
 
-    app.Session mockSession = mock(app.Session.class);
-    mockSession.role = Role.Admin;
-    when(req.getAttribute(app.Session.ATTRIBUTE_NAME)).thenReturn(mockSession);
+		HibernateUtil.setDAOFactory(fact);
+		org.hibernate.Session ssn = mock(org.hibernate.Session.class);
+		org.hibernate.Transaction transaction = mock(org.hibernate.Transaction.class);
+		SessionFactory sfact = mock(SessionFactory.class);
+		when(sfact.getCurrentSession()).thenReturn(ssn);
+		when(ssn.getTransaction()).thenReturn(transaction);
+		HibernateUtil.setFactory(sfact);
+	}
 
-    RoomType mockrt = mock(RoomType.class);
-    when(rtDAO.findById(1)).thenReturn(mockrt);
-    RoomType rt = RoomTypeController.getRoomType(1, req, res);
-    verify(rtDAO, times(1)).findById(1);
-    assertEquals(rt, mockrt);
-    
-  }
-  
-  @Test
-  public void RoomTypePostTest() {
-  
-	  app.Session mockSession = mock(app.Session.class);
-	  mockSession.role = Role.Admin;
-	  when(req.getAttribute(app.Session.ATTRIBUTE_NAME)).thenReturn(mockSession);
-	  
-	  RoomType mockrt = mock(RoomType.class);
-	  when(rtDAO.findById(1)).thenReturn(mockrt);
-	  RoomType rt = new RoomType("Lecture Hall");
-	  RoomTypeController.postRoomType(rt, req, res);
-	  verify(rtDAO, times(1)).findById(1);
-	  RoomType rt2 = RoomTypeController.getRoomType(1, req, res);
-	  verify(rtDAO, times(2)).findById(1);
-	  assertEquals(rt2, mockrt);
-	  
-  }
+	@Before
+	public void setup() {
+		res = new MockHttpServletResponse();
+		req = mock(MockHttpServletRequest.class);
+	}
+
+	@Test
+	public void RoomTypeGetTest() {
+
+		app.Session mockSession = mock(app.Session.class);
+		mockSession.role = Role.Admin;
+		when(req.getAttribute(app.Session.ATTRIBUTE_NAME)).thenReturn(mockSession);
+
+		RoomType mockrt = mock(RoomType.class);
+		when(rtDAO.findById(1)).thenReturn(mockrt);
+		RoomType rt = RoomTypeController.getRoomType(1, req, res);
+		verify(rtDAO, times(2)).findById(1);
+		assertEquals(rt, mockrt);
+
+	}
+
+
+	@Test
+	public void RoomTypePostTest() {
+
+		app.Session mockSession = mock(app.Session.class);
+		mockSession.role = Role.Admin;
+		when(req.getAttribute(app.Session.ATTRIBUTE_NAME)).thenReturn(mockSession);
+
+		RoomType mockrt = mock(RoomType.class);
+		when(mockrt.getId()).thenReturn(1);
+
+		//RoomType rt = new RoomType("Lecture Hall");
+		//when(rtDAO.makePersistent(rt)).thenReturn(rt);
+		Integer rPost = RoomTypeController.postRoomType(mockrt, req, res);
+
+		Integer check = 1;
+		assertEquals(rPost, check);
+
+	}
+
+	@Test
+	public void RoomTypeDeleteTest() {
+
+		app.Session mockSession = mock(app.Session.class);
+		mockSession.role = Role.Admin;
+		when(req.getAttribute(app.Session.ATTRIBUTE_NAME)).thenReturn(mockSession);
+
+		RoomType mockroomType = mock(RoomType.class);
+		when(rtDAO.findById(1)).thenReturn(mockroomType);
+		RoomTypeController.deleteRoomType(1, req, res);
+		verify(rtDAO, times(1)).makeTransient(mockroomType);
+	}
 
 }
