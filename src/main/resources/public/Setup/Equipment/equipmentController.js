355afc898656a4cf1338c4d50c48df2c129ccf0e
Seth Barrios <sethb157@yahoo.com>
2017-02-09 00:00:53 -0800

    Add table for data display to equipment partial

--- src/main/resources/public/Setup/Equipment/equipmentController.js
+++ src/main/resources/public/Setup/Equipment/equipmentController.js
@@ -1,4 +1,16 @@
-app.controller('equipmentController', ['$scope', '$state',
-  function ($scope, $state) {
+app.controller('equipmentController', ['$scope', '$state', '$http',
+  function ($scope, $state, $http) {
 
+    // Define equipment endpoint interface
+    fetchAllEquip = function() {
+        $http({
+            method: 'GET',
+            url: 'api/equip'
+        }).then(function success(response) {
+            $scope.equipment = response.data;
+        });
+    };
+
+    // Fetch equip on startup
+    fetchAllEquip();
   }]);
--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -1,5 +1,6 @@
 
 var app = angular.module('mainApp', [
    'ui.router',
-   'ui.bootstrap'
+   'ui.bootstrap',
+   'smart-table'
 ])
