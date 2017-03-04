3e14f0d8150f64fc9b5dfda89a4af12beeb92bdd
Johnathan Nicholson <johnathan.nicholson3@gmail.com>
2017-03-04 13:30:21 -0800

    add basic course pref front end, kinda ugly
    Signed-off-by: Johnathan Nicholson <johnathan.nicholson3@gmail.com>

--- src/main/resources/public/Pref/prefController.js
+++ src/main/resources/public/Pref/prefController.js
@@ -2,7 +2,7 @@ app.controller('prefController', ['$scope', '$state', 'login', '$http', 'notifyD
   function(scope, state, login, $http, nDlg, $uibM) {
     scope.coursePrefs = [];
     scope.addCoursePref = function() {
-        scope.coursePrefs.push({id: null, prof: {id: login.getUser().id}, course: null, level: 1})
+        scope.coursePrefs.unshift({id: null, prof: {id: login.getUser().id}, course: null, level: 1})
     }
     login.getUserPromise()
     .then(function(user) {
@@ -18,10 +18,14 @@ app.controller('prefController', ['$scope', '$state', 'login', '$http', 'notifyD
     })
 
     scope.postCoursePref = function(pref) {
-      $http.post("/api/pref/crs", pref)
-      .then(function(response) {
-        pref.id = response.data;
-      });
+      if (pref.id == null) {
+        $http.post("/api/pref/crs", pref)
+        .then(function (response) {
+          pref.id = response.data;
+        });
+      } else {
+        $http.put("/api/pref/crs/" + pref.id, pref)
+      }
     }
 
   }]);
