d7e165f37506be2234b1ba9761fbd0aebef1db8d
Seth Barrios <sethb157@yahoo.com>
2017-03-08 00:31:55 -0800

    Implement ability to update published status

--- src/main/resources/public/Schedule/Term/termController.js
+++ src/main/resources/public/Schedule/Term/termController.js
@@ -1,8 +1,10 @@
-app.controller('termController', ['$scope', '$state', '$http', 'login',
-function(scope, $state, $http, login) {
+app.controller('termController', ['$scope', '$state', '$http', 'login', 'notifyDlg',
+function(scope, $state, $http, login, notifyDlg) {
 
   // Only admin users can edit
   scope.showEdit = login.isAdmin();
+
+  scope.publishedOptions = [{name : "NO", val: 0}, {name : "YES", val: 1}];
 
   // Grabs all terms from the server
   scope.fetchAllTerms = function() {
@@ -15,6 +17,16 @@ function(scope, $state, $http, login) {
     })
     .catch(function error(response) {
       return notifyDlg.show($scope, "Could not fetch: " + response.status);
+    });
+  }
+
+  scope.updateTerm = function(term) {
+    $http.put('api/term/' + term.id, term)
+    .then(function success(response) {
+      return notifyDlg.show(scope, "Successfully updated published status.");
+    })
+    .catch(function error(response) {
+      return notifyDlg.show(scope, "Could not update published status: " + response.status);
     });
   }
 
